9a60193de300a00d336d0006d95479198e3b6ac0
Mallard <mallard.carlosm@gmail.com>
2022-11-30 09:54:01 -0600

    Agregado método de tabulaciones

--- Lenguaje.cs
+++ Lenguaje.cs
@@ -1,10 +1,10 @@
 // Carlos Mallard
 
 // Requerimiento 1.- Construir un método para escribir en el archivo lenguaje.cs identando el código-
-//                   "{" incrementa un tabulador, "}" decrementa un tabulador.
+//                   "{" incrementa un tabulador, "}" decrementa un tabulador. *
 
 // Requerimiento 2.- Declarar un atributo primeraProduccion de tipo string y actualizarlo con la primera
-//                   producción de la gramática.
+//                   producción de la gramática. 
 
 // Requerimiento 3.- La primera producción es pública, y el resto privadas.
 
@@ -17,8 +17,6 @@
 // Requerimiento 6.- Agregar el paréntesis izquierdo y derecho escapados en la matriz de transiciones.
 
 // Requerimiento 6.- Implementar el OR y la cerradura epsilon
-
-
 
 using System;
 using System.Collections.Generic;
@@ -35,6 +33,31 @@ namespace Generador
         public Lenguaje(string nombre) : base(nombre)
         {
             listaSNT = new List<string>();
+        }
+
+        public string calculadorTab(string cadenas)
+        {
+            string cadenaMod = cadenas;
+
+            string tabulacion = "";
+
+            int contadorTab = 0;
+
+            if (cadenas.Contains("{"))
+            {
+                contadorTab++;
+            }
+            if (cadenas.Contains("}"))
+            {
+                contadorTab--;
+            }
+            
+            for (int i = 0; i < contadorTab; i++)
+            {
+                tabulacion += "\t";
+            }
+            
+            return tabulacion + cadenaMod;
         }
 
         public void Gramatica()
@@ -58,37 +81,38 @@ namespace Generador
 
         private void Programa(string produccionPrincipal)
         {
-            programa.WriteLine("using System;");
-            programa.WriteLine("using System.IO;");
-            programa.WriteLine("using System.Collections.Generic;");
-            programa.WriteLine("namespace ");
-            programa.WriteLine("{");
-            programa.WriteLine("class " + produccionPrincipal);
-            programa.WriteLine("{");
-            programa.WriteLine("static void Main(string[] args)");
-            programa.WriteLine("{");
-            programa.WriteLine("try");
-            programa.WriteLine("{");
-            programa.WriteLine("instanciaObjeto();");
-            programa.WriteLine("GC.Collect();");
-            programa.WriteLine("}");
-            programa.WriteLine("catch (Exception e)");
-            programa.WriteLine("{");
-            programa.WriteLine("Console.WriteLine(e.Message);");
-            programa.WriteLine("}");
-            programa.WriteLine("}");
-            programa.WriteLine("static void instanciaObjeto()");
-            programa.WriteLine("{");
-            programa.WriteLine(produccionPrincipal + " a = new " + produccionPrincipal + "();");
-            programa.WriteLine("{");
-            programa.WriteLine("while (!a.FinArchivo())");
-            programa.WriteLine("{");
-            programa.WriteLine("a.NextToken();");
-            programa.WriteLine("}");
-            programa.WriteLine("}");
-            programa.WriteLine("}");
-            programa.WriteLine("}");
-            programa.WriteLine("}");
+
+            programa.WriteLine(calculadorTab("using System;"));
+            programa.WriteLine(calculadorTab("using System.IO;"));
+            programa.WriteLine(calculadorTab("using System.Collections.Generic;"));
+            programa.WriteLine(calculadorTab("namespace "));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab("class " + produccionPrincipal));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab("static void Main(string[] args)"));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab("try"));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab("instanciaObjeto();"));
+            programa.WriteLine(calculadorTab("GC.Collect();"));
+            programa.WriteLine(calculadorTab("}"));
+            programa.WriteLine(calculadorTab("catch (Exception e)"));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab("Console.WriteLine(e.Message);"));
+            programa.WriteLine(calculadorTab("}"));
+            programa.WriteLine(calculadorTab("}"));
+            programa.WriteLine(calculadorTab("static void instanciaObjeto()"));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab((produccionPrincipal + " a = new " + produccionPrincipal + "();")));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab("while (!a.FinArchivo())"));
+            programa.WriteLine(calculadorTab("{"));
+            programa.WriteLine(calculadorTab("a.NextToken();"));
+            programa.WriteLine(calculadorTab("}"));
+            programa.WriteLine(calculadorTab("}"));
+            programa.WriteLine(calculadorTab("}"));
+            programa.WriteLine(calculadorTab("}"));
+            programa.WriteLine(calculadorTab("}"));
         }
 
         private void cabecera()
@@ -163,5 +187,4 @@ namespace Generador
             return false;
         }
     }
-
 }
